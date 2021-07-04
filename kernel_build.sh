bf9115b1d90715fb573e1a812609a4a47a4c905f
Udit Karode <udit.karode@gmail.com>
2021-07-04 19:08:03 +0530

    epsilon: add custom build scripts

--- kernel_build.sh
+++ kernel_build.sh
@@ -14,39 +14,13 @@ ARCH="arm64"
 SUBARCH="arm64"
 export ARCH SUBARCH
 
+KERNEL_DIR="$PWD"
+
 KERNEL_IMG=$KERNEL_DIR/out/arch/$ARCH/boot/Image.gz
 KERNEL_DTBO=$KERNEL_DIR/out/arch/$ARCH/boot/dtbo.img
-
-TG_CHAT_ID="1139604865"
-TG_BOT_TOKEN="$BOT_API_KEY"
 # End config
 
 # Function definitions
-
-# tg_sendinfo - sends text through telegram
-tg_sendinfo() {
-	curl -s "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" \
-		-F parse_mode=html \
-		-F text="${1}" \
-		-F chat_id="${TG_CHAT_ID}" &> /dev/null
-}
-
-# tg_pushzip - uploads final zip to telegram
-tg_pushzip() {
-	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
-	curl -F document=@"$1"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
-			-F chat_id=$TG_CHAT_ID \
-			-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>" \
-			-F parse_mode=html &> /dev/null
-}
-
-# tg_failed - uploads build log to telegram
-tg_failed() {
-    curl -F document=@"$LOG"  "https://api.telegram.org/bot$TG_BOT_TOKEN/sendDocument" \
-        -F chat_id=$TG_CHAT_ID \
-        -F caption="$((DIFF / 60))m $((DIFF % 60))s" \
-        -F parse_mode=html &> /dev/null
-}
 
 # build_setup - enter kernel directory and get info for caption.
 # also removes the previous kernel image, if one exists.
@@ -85,32 +59,24 @@ build_kernel() {
 
 # build_end - creates and sends zip
 build_end() {
-
-	if ! [ -a "$KERNEL_IMG" ]; then
-        echo -e "\n> Build failed, sending logs to Telegram."
-        tg_failed
+    if ! [ -a "$KERNEL_IMG" ]; then
+        echo -e "\n> $KERNEL_IMG does not exist - Build failed."
         exit 1
     fi
 
     echo -e "\n> Build successful! generating flashable zip..."
-	cd "$AK_DIR" || echo -e "\nAnykernel directory ($AK_DIR) does not exist" || exit 1
-	git clean -fd
-	mv "$KERNEL_IMG" "$AK_DIR"/zImage
-	mv "$KERNEL_DTBO" "$AK_DIR"
-	curl https://android.googlesource.com/platform/external/avb/+/refs/heads/master/avbtool.py?format=TEXT | base64 --decode > avbtool.py
-	python3 avbtool.py add_hash_footer --image dtbo.img --partition_size=33554432 --partition_name dtbo
-	ZIP_NAME=$KERNELNAME-$1-$COMMIT_SHA-$(date +%Y-%m-%d_%H%M)-UTC
-	zip -r9 "$ZIP_NAME".zip ./* -x .git README.md ./*placeholder avbtool.py
+    cd "$AK_DIR" || echo -e "\nAnykernel directory ($AK_DIR) does not exist" || exit 1
+    rm -fv zImage
+    rm -fv dtbo.img
+    rm -fv zipsigner-4.0.jar
+    mv "$KERNEL_IMG" "$AK_DIR"/zImage
+    mv "$KERNEL_DTBO" "$AK_DIR"
+    [ ! -f "avbtool.py" ] && curl https://android.googlesource.com/platform/external/avb/+/refs/heads/master/avbtool.py?format=TEXT | base64 --decode > avbtool.py
+    python3 avbtool.py add_hash_footer --image dtbo.img --partition_size=33554432 --partition_name dtbo
+    ZIP_NAME="delta-mod-$(date +"%H:%M:%S")"
+    zip -r9 "$ZIP_NAME".zip 'anykernel.sh' 'dtbo.img' 'zImage' 'META-INF' 'tools'
 
-	# Sign zip if java is available
-	if command -v java > /dev/null 2>&1; then
-		curl -sLo zipsigner-4.0.jar https://github.com/baalajimaestro/AnyKernel3/raw/master/zipsigner-4.0.jar
-		java -jar zipsigner-4.0.jar "$ZIP_NAME".zip "$ZIP_NAME"-signed.zip
-		ZIP_NAME="$ZIP_NAME-signed.zip"
-	fi
-
-	tg_pushzip "$ZIP_NAME" "Time taken: <code>$((DIFF / 60))m $((DIFF % 60))s</code>"
-	echo -e "\n> Sent zip through Telegram.\n> File: $ZIP_NAME"
+    echo "$ZIP_NAME" "Time taken: $((DIFF / 60))m $((DIFF % 60))s"
 }
 
 # End function definitions
@@ -120,10 +86,10 @@ COMMIT_SHA=$(git rev-parse --short HEAD)
 KERNEL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 CAPTION=$(echo -e \
-"HEAD: <code>$COMMIT_SHA: </code><code>$COMMIT</code>
-Branch: <code>$KERNEL_BRANCH</code>")
+"HEAD: $COMMIT_SHA: $COMMIT
+Branch: $KERNEL_BRANCH")
 
-tg_sendinfo "-- Build Triggered --
+echo "-- Build Triggered --
 $CAPTION"
 
 # Build device 1
@@ -131,13 +97,3 @@ build_setup
 build_config $DEFCONFIG
 build_kernel
 build_end $DEVICE
-
-# Use stock panel dimentions for miui vendor based roms
-cd $KERNEL_DIR
-git am patches/0001-Revert-ARM64-dts-sweet-Decrease-physical-panel-dimen.patch
-
-# Build device 1 for MIUI
-build_setup
-build_config $DEFCONFIG
-build_kernel
-build_end ${DEVICE}_MIUI
--- kernel_setup.sh
+++ kernel_setup.sh
@@ -7,27 +7,22 @@
 # Assumes required packages are already installed
 
 # Config
-CURRENT_DIR="$(pwd)"
-KERNELNAME="Delta"
-KERNEL_DIR="$CURRENT_DIR"
-AK_REPO="https://github.com/TheStaticDesign/AnyKernel3"
-AK_BRANCH="sweet"
-AK_DIR="$HOME/AnyKernel3"
-TC_DIR="$HOME/proton-clang"
+KERNELNAME="Delta-mod"
+KERNEL_DIR="$PWD"
+
+export AK_DIR="$PWD/flasher"
+export TC_DIR="$PWD/../sweet_assets/proton-clang"
 # End Config
+
+# create assets dir
+[ ! -d "$PWD/../sweet_assets" ] && mkdir ../sweet_assets
 
 # clone_tc - clones proton clang to TC_DIR
 clone_tc() {
 	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git $TC_DIR
 }
 
-# Clones anykernel
-clone_ak() {
-	git clone $AK_REPO $AK_DIR -b $AK_BRANCH
-}
 # Actually do stuff
-clone_tc
-clone_ak
+[ ! -d "$TC_DIR" ] && clone_tc
 
-# Run build script
-. ${CURRENT_DIR}/kernel_build.sh
+./kernel_build.sh
